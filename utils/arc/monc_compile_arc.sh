4da40fb0ef4afac331392d17170c19d47c6166b0
Elfe1107 <85030544+Elfe1107@users.noreply.github.com>
2021-08-11 15:26:45 +0100

    Update monc_compile_arc.sh

--- utils/arc/monc_compile_arc.sh
+++ utils/arc/monc_compile_arc.sh
@@ -5,7 +5,8 @@
 module purge
 module load user
 module switch intel gnu/8.3.0
-#module switch openmpi mvapich2
+module switch openmpi mvapich2
 module load fftw netcdf hdf5 fcm
 
-fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg
+#fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg
+fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg -f fcm-make/casim_socrates.cfg
